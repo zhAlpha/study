4d9f06c66fefa80bc3b0689eafd78d31fd5691a2
zhuhui1212 <644526179@qq.com>
2019-11-01 11:46:50 +0800

    cppstudy

---
+++ cppstudy/cmake-build-debug/CMakeFiles/cppstudy.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cppstudy.dir/main.cpp.o"
+  "cppstudy"
+  "cppstudy.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cppstudy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
